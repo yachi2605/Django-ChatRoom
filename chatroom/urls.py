46f41026f8ff42cf458bad097924482c247aa37b
yachi2605 <yachi2605@gecg28.ac.in>
2023-03-31 21:30:39 +0530

    not done yet

---
+++ chatroom/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from core import urls
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls'))
+]
+
+
